8bde0d0f7e110293699460450fdf930a13c09b52
Fabián Ramos <fabian.ramos01@uptc.edu.co>
2021-08-30 22:47:55 -0500

    Added tests for users and chats

--- chat_app/apps/chat/tests.py
+++ chat_app/apps/chat/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class TestUrlAccess(TestCase):
+
+    def setUp(self):
+        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
+
+    def test_access_login_with_user_logged(self):
+        response = self.client.post('/chat/jobsity')
+        self.assertEqual(response.status_code, 301)
--- chat_app/apps/users/tests.py
+++ chat_app/apps/users/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from selenium.webdriver import Firefox
+
+from apps.users.models import CustomUser
+
+class TestUsers(TestCase):
+
+    def setUp(self):
+        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
+        CustomUser.objects.create_user(username='Jose', password='X9YNyH17A234v')
+
+    def test_create_user(self):
+        response = self.client.post('/register', {'username': 'Carlos', 'password1': 'X9YNyH17A9!b', 'password2': 'X9YNyH17A9!b'})
+        self.assertEqual(response.status_code, 302)
+
+    def test_login_user(self):
+        response = self.client.post('/', {'username': 'Jose', 'password1': 'X9YNyH17A234v'})
+        print(response.get_host())
+        self.assertEqual(response.status_code, 302)
